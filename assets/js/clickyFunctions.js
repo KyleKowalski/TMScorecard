6407c74eb50a20791f4d1e1de07a5e70bd783ec7
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2018-01-20 07:32:35 -0700

    template for single clicky function - now to wire in buttons

--- assets/js/clickyFunctions.js
+++ assets/js/clickyFunctions.js
@@ -12,4 +12,20 @@ $(document).ready(function() {
         $("#trTotal1").text(totalTR);
     });
 
+    $(".clicky").click(function(){
+        console.log(this);
+        let myTarget="#"+$(this).attr("myTargetValue")+$(this).attr("myTargetId");
+        console.log(myTarget);
+        let myValue =$(myTarget).text();
+        console.log(`Current value: ${myValue}`);
+        if ($(this).attr("math") === "plus") {
+            myValue++
+        }
+        else {
+            myValue--
+        }
+        $(myTarget).text(myValue);
+        // TODO some sort of logging here
+    })
+
 }) // End document.ready
